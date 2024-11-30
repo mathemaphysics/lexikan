480aa15a265d0a8953088a409ca82eb76d0c949f
Ryan P. Daly <mathemaphysics@gmail.com>
2024-11-30 07:11:27 +0000

    Added exception handling in the main app.

--- app/lexikan.cpp
+++ app/lexikan.cpp
@@ -8,6 +8,8 @@
 // std
 #include <vector>
 #include <string>
+#include <iostream>
+#include <sstream>
 
 // Shorthand names
 namespace po = boost::program_options;
@@ -17,19 +19,36 @@ int main(int argc, char** argv)
 {
 	// Options examples
 	int opt;
+	auto logger = sp::stderr_color_mt("__FILE__");
 
 	// Set up the program options
 	po::options_description options("Allowed options");
 	options.add_options()
 		("help", "Outputs this help message")
 		("verbose", "Causes us to emit screeches")
-		("help", "produce help message")
-    	("optimization", po::value<int>(&opt)->default_value(10), "optimization level")
-    	("include-path,I", po::value<std::vector<std::string>>(), "include path")
+		("optimization", po::value<int>(&opt)->default_value(10), "optimization level")
+		("include-path,I", po::value<std::vector<std::string>>(), "include path")
 		("input-file", po::value< std::vector<std::string>>(), "input file")
 		;
 	
 	po::variables_map varmap;
-	po::store(po::parse_command_line(argc, argv, options), varmap);
-	po::notify(varmap);
+	try
+	{
+		po::store(po::parse_command_line(argc, argv, options), varmap);
+		po::notify(varmap);
+	}
+	catch (po::unknown_option& ex)
+	{
+		logger->error("Unknown option: {}", ex.what());
+		options.print(std::cerr);
+	}
+	catch (po::invalid_option_value& ex)
+	{
+		logger->error("Bad option: {}", ex.what());
+		options.print(std::cerr);
+	}
+
+	logger->info("Welcome to lexikan!");
+
+	return 0;
 }
